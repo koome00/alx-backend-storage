9bcaff875f17e03ecf7debe4c2228ef5669931e3
koome <koomemc@gmail.com>
2024-03-21 19:08:23 +0000

    fix syntax

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,8 @@
+-- creates table with specified attributes
 -- creates table users
 CREATE TABLE users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255) NOT NULL
 );
+
